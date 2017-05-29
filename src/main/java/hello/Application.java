359921fe5368d25548da6c14f227a812002350b4
DavidJohnSmith <dongxiedevelop@gmail.com>
2017-05-29 12:37:07 +0800

    spring boot maven add

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dongxie on 2017/5/7.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
